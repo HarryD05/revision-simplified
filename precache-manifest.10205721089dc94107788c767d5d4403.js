1e8f3eb90582638ae2633d9f554c68425ce5ee29
Harry Davis <harrytobydavis@gmail.com>
2020-07-11 19:31:34 +0100

    Updates

---
+++ precache-manifest.10205721089dc94107788c767d5d4403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bef4fd98de05535f38bb31b70c9be03",
+    "url": "/revision-simplified/index.html"
+  },
+  {
+    "revision": "4e5b83414334c1def883",
+    "url": "/revision-simplified/static/css/main.5c697822.chunk.css"
+  },
+  {
+    "revision": "261c242a157b88e1a44c",
+    "url": "/revision-simplified/static/js/2.ebd58866.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/revision-simplified/static/js/2.ebd58866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5b83414334c1def883",
+    "url": "/revision-simplified/static/js/main.96c04246.chunk.js"
+  },
+  {
+    "revision": "9bd6c727169afc7c99c8",
+    "url": "/revision-simplified/static/js/runtime-main.c37bf463.js"
+  }
+]);
