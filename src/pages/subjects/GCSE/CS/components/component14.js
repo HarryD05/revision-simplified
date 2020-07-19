5ea35bf43b3fdfe7b26bcb5b45c48d6881e83401
Harry Davis <harrytobydavis@gmail.com>
2020-07-19 16:05:14 +0100

    Added component 1.4 & 1.5 content (GCSE CS)

--- src/pages/subjects/GCSE/CS/components/component14.js
+++ src/pages/subjects/GCSE/CS/components/component14.js
@@ -3,20 +3,194 @@ import React from 'react';
 const Component14 = () => {
   return (
     <div id="comp1_4" className="topic">
-      <h4>Component 1.4 - Wired & wireless networks</h4>
+      <h4>Component 1.4- Wired & wireless networks</h4>
       
       <div className="key-words">
       <p className="heading">KEY WORDS</p>
       <ul>
-        <li><b>TERM</b> - definition</li>
+        <li><b>Router</b> - network hardware responsible for packet switching (connection to large WANs)</li>
+        <li><b>Bandwidth</b> - amount of data that <em>can</em> be transmitted within a given period (like size of a road)</li>
+        <li><b>Throughput</b> - amount of data that <em>is</em> transmitted within a given period (like busyness of a road)</li>
+        <li><b>Website</b> - a collection of webpages linked via. hyperlinks</li>
       </ul>
       </div>
 
-      <p className="heading">SUBTOPIC</p>
+      <p className="heading">Networks</p>
       <ul>
-        <li>PLACEHOLDER</li>
+        <li>2 main types:</li>
+        <ul>
+          <li><b>LAN (Local Area Network)</b> - network covering a small geographical area on a single site, where all hardware is owned by the user e.g. a school, home.</li>
+          <li><b>WAN (Wide Area Network</b> - network covering a large geographical area (connecting multiple LANs) where infrastructure is often hired from telecommunication companies e.g. internal business or the Internet</li>
+        </ul>
+
+        <li>Benefits of networks</li>
+        <ul>
+          <li>Easy communication (file sharing etc.)</li>
+          <li>Sharing hardware such as printers</li>
+          <li>Centrally installed & updated software</li>
+          <li>User accounts stored centrally (remote access)</li>
+        </ul>
+
+        <li>Factors affecting network performance</li>
+        <ul>
+          <li><b>Bandwidth </b> - the amount of data that can be transferred in a given period (e.g. the number of lanes a road has), mbps (megabits per second)</li>
+          <li><b>Number of users & usage </b> - bandwidth is shared between users causing congestion & data collision</li>
+          <li><b>Connection type </b></li>
+          <ul>
+            <li>wires are faster (& more reliable) than wireless connections, & fibre optic faster than copper.</li>
+            <li>Wireless performance impacted by signal quality (effected by range & physical obstacles etc.)</li>  
+          </ul>
+
+          <li><b>Interference </b> - electromagnetic interference may corrupt data during transmission</li>
+          <li><b>Obstacles </b> - physical objects prevent/impact radio wave transmission (wireless)</li>
+        </ul>
+
+        <li>LANs require certain hardware</li>
+        <ul>
+          <li><b>Router </b> - responsible for data transmission between networks (LAN-LAN, LAN-WAN), packet switching</li>
+          <ul>
+            <li>Most home routers connect the LAN to a WAN (Internet) as well as switch & Wireless Access Point</li>
+          </ul>
+
+          <li><b>Switch </b> - connects devices on a LAN, control data transfer (each “bit” of data is a “frame”) within the LAN using each devices MAC address </li>
+          <li><b>Network Interface Card (NIC) </b> - hardware allowing a device to connect to a network can be wired (Ethernet) or wireless (Wi-Fi)</li>
+          <li><b>Transition media </b> - Ethernet cables (e.g. Cat5e, fibre optics), wireless</li>
+          <li><b>Wireless Access Point (WAP) </b> - like a wireless switch allowing devices to connect to other devices within a LAN </li>
+        </ul>
+
       </ul>
-      </div>
+
+      <p className="heading">Network Models</p>
+      <ul>
+        <li><b>Client-Server</b></li>
+        <ul>
+          <li>Managed by a central server, devices connected to the server are clients.</li>
+          <li>Server stores files, user details & software while receiving a responding to requests from clients (may authenticate using passwords etc.)</li>
+          <li>Clients send requests for data to the server</li>
+          <div className="advantages">
+            <li><b>Advantages</b></li>
+            <ul>
+              <li>Easy to maintain files</li>
+              <li>Backups & software installs/updates easy to do centrally</li>
+              <li>Network security easy to control centrally</li>
+            </ul>
+          </div>
+
+          <div className="disadvantages">
+            <li><b>Disadvantages</b></li>
+            <ul>
+              <li>If the server fails all clients lose access to the network</li>
+              <li>Server may be overloaded with too many simultaneous requests</li>
+              <li>Set up & maintenance expensive (often specialists required)</li>
+            </ul>
+          </div>
+        </ul>
+
+        <li><b>Peer-to-peer</b></li>
+        <ul>
+          <li>All devices (nodes) equal, connect to each other</li>
+          <li>Computers store their own files & send them to others upon request</li>
+          <div className="advantages">
+            <li><b>Advantages</b></li>
+            <ul>
+              <li>Easy maintenance</li>
+              <li>No server dependence </li>
+            </ul>
+          </div>
+
+          <div className="disadvantages">
+            <li><b>Disadvantages</b></li>
+            <ul>
+              <li>No central management – backups more complicated, all done individually</li>
+              <li>Nodes slow down while processing requests from other devices</li>
+              <li>Files only able to be accessed if the device that stores them is online</li>
+            </ul>
+          </div>
+        </ul>
+      </ul>
+
+      <p className="heading">Internet</p>
+      <ul>
+        <li>The internet is a WAN connecting networks globally (based around the TCP/IP protocol)</li>
+        <li><b>Domain Name Server (DNS)</b></li>
+        <ul>
+          <li>Translates a website’s domain name (from its URL) into its IP address </li>
+          <li>If a DNS miss occurs (IP not found) a larger DNS server contacted</li>
+        </ul>
+
+        <li><b>Hosting</b></li>
+        <ul>
+          <li>When a business stores files for another organisation – traditionally webhosting on the Internet</li>
+        </ul>
+
+        <li><b>How web browsers work</b></li>
+        <ul>
+          <li>The website is hosted on a webserver.</li>
+          <li>The web browser sends the websites URL to the DNS which returns the website’s (or webserver’s) IP address.</li>
+          <li>The web browser then sends a request to the webserver for the website linked to the IP address</li>
+        </ul>
+        
+        <li><b>Internet traffic</b></li>
+        <ul>
+          <li>WWW (websites), email, multiplayer games, video streaming</li>  
+        </ul>
+
+        <li><b>The Cloud</b></li>
+        <ul>
+          <li>Remotely accessed storage & services via. The Internet (form of hosting) - Modern use of hosting</li>
+          <li>Data can be stored on a server anywhere</li>
+          <li>Uses of the Cloud</li>
+          <ul>
+            <li>Server hosting</li>
+            <li>Storage of files</li>
+            <li>Hosting applications</li>
+            <li>Streaming</li>
+          </ul>
+
+          <div className="advantages">
+            <li><b>Advantages</b></li>
+            <ul>
+              <li>Access from anywhere</li>
+              <li>Easy to increase capacity</li>
+              <li>No need to buy expensive extra hardware</li>
+              <li>Cloud host provides backup services</li>
+              <li>Perfect for travellers & low-powered device</li>
+              <li>Low cost – only pay for the services you use</li>
+            </ul>
+          </div>
+
+          <div className="disadvantages">
+            <li><b>Disadvantages</b></li>
+            <ul>
+              <li>Requires an internet connection</li>
+              <li>Dependent on host for backups</li>
+              <li>Data vulnerable to hacking</li>
+              <li>Ownership of data is unclear (host may claim copyright etc.)</li>
+              <li>Hardware may have more capacity</li>
+              <li>Access speeds impacted by network traffic</li>
+              <li>Costs can be changed whenever by the host</li>
+            </ul>
+          </div>
+        </ul>
+      </ul>
+
+      <p className="heading">Virtual Networks</p>
+      <ul>
+        <li>Software-based network, some of the physical network’s bandwidth sectioned off </li>
+        <li>Several virtual networks can run on the same physical network (share hardware) - cheaper</li>
+        <li><b>Virtual Private Network (VPN)	</b></li>
+        <ul>
+          <li>Type of virtual network were data can be sent securely (a tunnel is set up between communicating devices with encryption)</li>
+          <li>Allows remote access</li>
+        </ul>
+
+        <li><b>Virtual LAN (VLAN)</b></li>
+        <ul>
+          <li>Setup a LAN into several separate networks using the same hardware </li>
+          <li>E.g. split into departments so security is increased as malware is contained </li>
+        </ul>
+      </ul>
+    </div>
   )
 }
 
--- src/pages/subjects/GCSE/CS/components/component15.js
+++ src/pages/subjects/GCSE/CS/components/component15.js
@@ -8,15 +8,264 @@ const Component15 = () => {
       <div className="key-words">
       <p className="heading">KEY WORDS</p>
       <ul>
-        <li><b>TERM</b> - definition</li>
+        <li><b>Latency</b> - how long it takes a message to travel from one device to another across a network (delay caused by interference)</li>
+        <li><b>Error rate</b> - how reliably packets are transmitted </li>
+        <li><b>Jitter</b> - effect of congestion on receiving of packets (delay between packets causing buffering)</li>
+        <li><b>Ethernet</b> - an array of networking technologies used in LANs for physical connections</li>
+        <li><b>Protocol</b> - set of rules for how devices communicate (e.g. language, size of packets, what to do with corrupted packets, what network ports to use)</li>
+        <li><b>Layers</b> - group of protocols with a similar function</li>
       </ul>
       </div>
 
-      <p className="heading">SUBTOPIC</p>
+      <p className="heading">Network Topologies</p>
       <ul>
-        <li>PLACEHOLDER</li>
+        <li><b>Star</b></li>
+        <ul>
+          <li>o	All devices connected to a central switch</li>
+          <div className="advantages">
+            <li><b>Advantages</b></li>
+            <ul>
+              <li>If a device or cable fails, the network isn’t impacted</li>
+              <li>Adding devices is easy - only 1 wire required</li>
+              <li>All devices can transmit data & once without data collision</li>
+              <li>Security is central (firewall at the switch)</li>
+            </ul>
+          </div>
+
+          <div className="disadvantages">
+            <li><b>Disadvantages</b></li>
+            <ul>
+              <li>If the switch fails, the whole network fails</li>
+            </ul>
+          </div>
+        </ul>
+
+        <li><b>Mesh</b></li>
+        <ul>
+          <li>Devices are connected to other devices in the network (decentralised)</li>
+          <li>Data sent along the fasted route </li>
+          <li>Full mesh - Every device connected to all other</li>
+          <li>Partial mesh - Not all devices connect to all other</li>
+          <div className="advantages">
+            <li><b>Advantages</b></li>
+            <ul>
+              <li>No single point of failure, any failure a different route can be used</li>
+              <li>Quick communication – so many connections & routes (avoid data collision)</li>
+              <li>Well suited to wireless networks (or small wired)</li>
+            </ul>
+          </div>
+
+          <div className="disadvantages">
+            <li><b>Disadvantages</b></li>
+            <ul>
+              <li>Expensive due to wiring (well-suited to wireless)</li>
+              <li>Hard to find faults (so many connections)</li>
+            </ul>
+          </div>
+        </ul>
+
+        <li><b>Others</b><em>(Not in specification)</em></li>
+        <ul>
+          <li><b>Ring</b> - device connected to the next and data flows in 1 direction (no collisions) only one device sends data at once and it travels through many computers</li>
+          <li><b>Bus</b> - all devices connection to a single backbone cable, data sent bi-directionally (lots of collision)</li>
+        </ul>
       </ul>
-      </div>
+
+      <p className="heading">Wi-Fi</p>
+      <ul>
+        <li>Wireless networks use radio waves for communication</li>
+        <li><b>Frequency</b> - there are 2 frequency bands</li>
+        <ul>
+          <li>2.4GHz - better range, less affected by obstacles</li>
+          <li>5GHz – faster over short distances</li>
+        </ul>
+
+        <li><b>Channels </b> - reduce interference, increase amount of data that can be sent</li>
+        <ul>
+          <li>The two 1 GHz bands are split into channels</li>
+          <li>2.4GHz has 13 overlapping channels (1, 6, 11 don’t so) overlap causes interference</li>
+          <li>5 GHz has 24 non-overlapping channels, so interference is less likely</li>
+        </ul>
+
+        <li><b>Encryption</b></li>
+        <ul>
+          <li>o	Data is translated into code only the correct key can access     </li>
+          <li>o	Essential for secure data transfer         </li>
+        </ul>
+
+        <li><b>Security</b> <em>(not in specification)</em></li>
+        <ul>
+          <li>Wired Equivalent Privacy (WEP) – weak, password required (no encryption)</li>
+          <li>Wi-Fi Protected Access (WPA) – password & encryption </li>
+          <li>Wi-Fi Protected Access Version 2 (WPA V2) – improved encryption algorithm</li>
+        </ul>
+      </ul>
+
+      <p className="heading">Ethernet</p>
+      <ul>
+        <li>Cables used to connect devices in a LAN - Cat & Coaxial use electrical signals </li>
+        <li>Cat 5, 5e, 6 are twisted pair cables containing 4 copper wires twisted to reduce interference</li>
+        <li>Coaxial cables use a single copper wire surrounded by plastic and a metallic mesh </li>
+        <li>Fibre optic cables use light – high speed (expensive) no interference so can travel long distances</li>
+      </ul>
+
+      <p className="heading">Protocols</p>
+      <ul>
+        <li><b>IP addresses</b> - ensure devices can communicate over the internet </li>
+        <ul>
+          <li>Used when sending data via. TCP/IP over the Internet</li>
+          <li>IP addresses aren’t linked to hardware</li>
+          <li>Private IP addresses – within a LAN (switches)</li>
+          <li>Public IP addresses – connecting LAN to a WAN (routers)</li>
+          <li>Static IP addresses – permanent used to connect printers to LANs & hosting (big) websites, VERY expensive</li>
+          <li>Dynamic IP addresses – assigned to a device each time it connects to a network, cost effective and can be reused</li>
+          <li>IPv4 (32-bit or 4 denary number) or IPv6 (128-bit or 8 hexadecimal)</li>
+        </ul>
+
+        <li><b>MAC addresses</b></li>
+        <ul>
+          <li>Every network-enabled device has a unique MAC address, pre-manufactured </li>
+          <li>48 or 64 bit (represented as a hexadecimal number)</li>
+          <li>1st half, the manufacturer details – 2nd half, individual device</li>
+          <li>Used by the Ethernet protocol within LANs – used by the Switch (for local data transmission)</li>
+        </ul>
+
+        <li><b>Protocols</b></li>
+        <ul>
+          <li><b>TCP/IP </b></li>
+          <ul>
+            <li>Transmission Control Protocol/Internet Protocol</li>
+            <li>TCP – sets up packet switching rules</li>
+            <li>IP – responsible for packet switching</li>
+          </ul>
+
+          <li><b>HTTP </b></li>
+          <ul>
+            <li>Hyper Text Transfer Protocol</li>
+            <li>Used by web browsers to communicate with web servers & access websites</li>
+            <li>Using requests & responses</li>
+          </ul>
+
+          <li><b>HTTPS </b></li>
+          <ul>
+            <li>Hyper Text Transfer Protocol Secure</li>
+            <li>Secured HTTPs – using encryption (secure socket layer, SSL)</li>
+            <li>E.g. for online banking</li>
+          </ul>
+
+          <li><b>FTP </b></li>
+          <ul>
+            <li>File Transfer Protocol</li>
+            <li>Access & move files between devices on a network</li>
+          </ul>
+
+          <li><b>POP </b></li>
+          <ul>
+            <li>Post Office Protocol</li>
+            <li>Retrieve emails from web servers (holds email until downloaded)</li>
+            <li>Asynchronous - emails can be stored on a single device</li>
+          </ul>
+
+          <li><b>IMAP (Newer than POP but less features)</b></li>
+          <ul>
+            <li>Internet Message Access Protocol</li>
+            <li>Retrieves emails from web servers (holds email until user deletes it)</li>
+            <li>Synchronous - emails can be stored on many devices</li>
+            <li>Used by web-based email</li>
+          </ul>
+
+          <li><b>SMTP</b></li>
+          <ul>
+            <li>Simple Mail Transfer Protocol</li>
+            <li>Used to send emails & transfer emails between servers</li>
+          </ul>
+
+          <li>To remember difference between IMAP & SMTP - just think 'S for send' in SMTP</li>
+        </ul>
+      </ul>
+
+      <p className="heading">Layers</p>
+      <ul>
+        <li>Layers are self-contained, but each layer serves the layer above it e.g. sending an email on Layer 4 triggers Layer 3 then 2 then 1.</li>
+        <li><b>Layers in the TCP/IP stack (ATIN/ATIL)</b> - what they do & the protocols included within them</li>
+        <ul>
+          <li><b>Application layer</b></li>
+          <ul>
+            <li>Allows access to network resources e.g. web browsers & mail services</li>
+            <li>FTP, HTTP(S), SMTP, IMTP (or POP)</li>
+          </ul>
+
+          <li><b>Transport layer</b></li>
+          <ul>
+            <li>Setting up communication, Handshaking – decide packet size, language, error checking</li>
+            <li>Splitting or joining of packets</li>
+            <li>TCP</li>
+          </ul>
+
+          <li><b>Internet  layer</b></li>
+          <ul>
+            <li>Where packets are made & sent </li>
+            <li>Reading or setting packet addresses</li>
+            <li>IP</li>
+          </ul>
+
+          <li><b>Network/Link layer</b></li>
+          <ul>
+            <li>Where data is sent</li>
+            <li>Ethernet Protocol – defines use of physical connections to send data</li>
+            <li>Wi-fi Protocol – defines use of radio waves to send data</li>
+          </ul>
+        </ul>
+
+        <div className="advantages">
+          <li><b>Advantages of using layers</b></li>
+          <ul>
+            <li>Breaks communication into manageable pieces, helps concentrate on 1 area</li>
+            <li>Self-contained so can be edited, without affecting other layers</li>
+            <li>Set rules for each layer force companies to make universal hardware & software</li>
+          </ul>
+        </div>
+      </ul>
+
+      <p className="heading">Packet Switching</p>
+      <ul>
+        <li>Controlled by the router directing data onto the Internet (or any WAN from LAN) using IP addresses</li>
+        <li>When sending data packet switching is used (e.g. opening a link on a web browser)</li>
+        <li><b>Process</b></li>
+        <ul>
+          <li>Request split into equally sized packets (the data in the packet is called the payload)</li>
+          <li>Packet labelled with a header:</li>
+          <ul>
+            <li>Sender & receiver IP address</li>
+            <li>Number (order) in bundle & number of packets in bundle</li>
+            <li>Checksum number (validation)</li>
+          </ul>
+
+          <li>Router sends packets across network</li>
+          <li>Host server receives packets</li>
+          <li>Server checks if all packets received (and checksum digits match)</li>
+          <li>If not...</li>
+          <ul>
+            <li>Time out error sent to request a resend of packets (if not received after a given time)</li>
+          </ul>
+
+          <li>If they have...</li>
+          <ul>
+            <li>packets put in order</li>
+            <li>request process by host (e.g. the webserver which would then send the website to the browser for it to be displayed on the client's screen)</li>
+          </ul>
+        </ul>
+        <li>Packets have 2 parts: the payload (data) & header (the 5 labels)</li>
+
+        <div className="advantages">
+          <li><b>Advantages</b></li>
+          <ul>
+            <li>Allows data to be sent efficiently (data split into packets and can reach the host with different routes avoiding congestion)</li>
+          </ul>
+        </div>
+        
+      </ul>
+    </div>
   )
 }
 
